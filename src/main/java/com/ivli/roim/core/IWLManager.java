a74aff0d8b6153f2a272bbe2cb22be88e39b9309
likhachev <likhachev@wks584.elektron.spb.su>
2016-02-25 10:22:37 +0300

    fixed building error in TimeSliceVector

--- src/main/java/com/ivli/roim/core/IWLManager.java
+++ src/main/java/com/ivli/roim/core/IWLManager.java
@@ -6,25 +6,18 @@ package com.ivli.roim.core;
  *
  * @author likhachev
  */
-public interface IWLManager extends Transformation {           
-    
-    void    setWindow(Window aNewWindow);
-    Window  getWindow();
-    
-    void    setRange(Range aNewRange);
-    Range   getRange();
-    
-   
-    void    openLUT(String aLutFileName);
-   
-    void    setInverted(boolean aInverted);
+public interface IWLManager extends Transformation {               
+    void setWindow(Window aNewWindow);
+    Window getWindow();    
+    void setRange(Range aNewRange);
+    Range getRange();       
+    void openLUT(String aLutFileName);   
+    void setInverted(boolean aInverted);
     boolean isInverted();
-    void    setLinear(boolean aLinear);
+    void setLinear(boolean aLinear);
     boolean isLinear();   
     void lockRange(boolean aLock);
-    void lockWindow(boolean aLock);
-    
-
+    void lockWindow(boolean aLock);    
      //TODO: 
     Curve getCurve();
 }
