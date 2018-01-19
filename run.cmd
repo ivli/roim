d84b5cdedae6400cf8f11df2efe92b890854682b
Igor Likhachev <likhachev@elektron.spb.su>
2018-01-19 13:56:13 +0300

    introducing modular architecture

--- run.cmd
+++ run.cmd
@@ -14,7 +14,7 @@ goto fine
 
 rem java -Djava.ext.dirs=.\spi\dicom\target;.\spi\bitmap\target;.\roim\target;.\spi\dicom\target\dependency;.\roim\target\dependency;.\apps\dynamic\target;.\apps\tomo\target; -cp ;.\apps\roim\target\roim-0.1.jar com.ivli.roim.Roim d:\images\composite.jpg
 
-java -Djava.ext.dirs=.\view\target;.\view\target\dependency;.\spi\dicom\target;.\roim\target;.\spi\dicom\target\dependency;.\roim\target\dependency;.\apps\dynamic\target;.\apps\tomo\target; -cp ;.\apps\dynamic\target\dynamic-0.1.jar com.ivli.roim.DYNAMIC 
+java -Djava.ext.dirs=.\view\target;.\view\target\dependency;.\spi\dicom\target;.\core\target;.\spi\dicom\target\dependency;.\roim\target\dependency;.\apps\dynamic\target;.\apps\tomo\target; -cp ;.\apps\dynamic\target\dynamic-0.1.jar com.ivli.roim.DYNAMIC 
 rem D:\images\dyn.dcm
 
 :fine
